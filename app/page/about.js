e75a8b83a8dd1931eee7b2e095ee9836ecdf1cff
Gilles Bertrand Tchako Ngangue <ngangueg@gmail.com>
2023-10-02 23:19:36 -0400

    Ajout pages et composants

---
+++ app/page/about.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import 'bootstrap/dist/css/bootstrap.css'
+import styles from './page.module.css'
+
+export default function About(){
+    return (
+        <>
+            <h1>About</h1>
+        </>
+    )
+}
