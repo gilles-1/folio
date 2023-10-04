5b7a13e849f48fa69e20cfed7f7bfb9088d299d3
Gilles Bertrand Tchako Ngangue <ngangueg@gmail.com>
2023-10-04 00:09:47 -0400

    Ajout Tailwindcss

--- app/page.js
+++ app/page.js
@@ -1,14 +1,28 @@
 import Image from 'next/image'
-import 'bootstrap/dist/css/bootstrap.css'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+
+config.autoAddCss = false;
+
 import styles from './page.module.css'
 import Head from 'next/head'
 import Header from './components/header'
 
+
+
 export default function Home(){
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <>
     <Head>
       <title>Accueil</title>
+    
+
     </Head>
     <Header/>
     <h1>Accueil</h1>
